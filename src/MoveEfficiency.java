3a27fa0088a514cafb7bbdcd2dc3caf4da2af925
keklol5050 <kek.lol5050@gmail.com>
2024-02-17 14:32:41 +0200

    Initial

---
+++ src/MoveEfficiency.java
@@ -0,0 +1,22 @@
+public class MoveEfficiency implements Comparable<MoveEfficiency> {
+    private int numberOfEmptyTiles;
+    private int score;
+    private Move move;
+
+    public MoveEfficiency(int numberOfEmptyTiles, int score, Move move) {
+        this.numberOfEmptyTiles = numberOfEmptyTiles;
+        this.score = score;
+        this.move = move;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    @Override
+    public int compareTo(MoveEfficiency o) {
+        int emptyTilesCompare = Integer.compare(numberOfEmptyTiles, o.numberOfEmptyTiles);
+        if (emptyTilesCompare!=0) return emptyTilesCompare;
+        return Integer.compare(score, o.score);
+    }
+}
